9ef0ef40e5e294683bfa241a611c13a1ab8b275e
Sebastián Monía <sebastian.monia@starz.com>
2019-08-13 06:24:58 -0600

    First working version

---
+++ awscli-capf.el
@@ -0,0 +1,196 @@
+(require 'cl)
+
+;;(add-to-list 'completion-at-point-functions 'awscli-capf)
+(defconst accapf--script-dir (file-name-directory load-file-name)  "The directory from which the package loaded.")
+(defconst accapf--data-file (expand-file-name "awscli-capf-docs.el" accapf--script-dir) "Location of the file with the help data.")
+(defvar accapf--services-info nil "Names and docs of all services, commands and options of the AWS CLI.")
+(defvar accapf--global-options-info nil "Top level options of the AWS CLI.")
+
+(cl-defstruct (accapf--service (:constructor accapf--service-create)
+                               (:copier nil))
+  name commands docs)
+
+(cl-defstruct (accapf--command (:constructor accapf--command-create)
+                               (:copier nil))
+  name options docs)
+
+(cl-defstruct (accapf--option (:constructor accapf--option-create)
+                               (:copier nil))
+  name type docs)
+
+(defun accapf--store-data-in-file (records)
+  "Save RECORDS in `accapf--data-file'."
+  (with-temp-buffer
+    (insert (prin1-to-string records))
+    (write-file accapf--data-file)
+    (message "awscli-capf - updated completion data")))
+
+(defun accapf--read-data-from-file ()
+  "Load the completion data stored in `accapf--data-file'."
+  (with-temp-buffer
+    (insert-file-contents accapf--data-file)
+    (let ((all-data (read (buffer-string))))
+      (setq accapf--services-info (first all-data))
+      (setq accapf--global-options-info (second all-data))
+      (message "awscli-capf - loaded completion data"))))
+
+(defun awscli-capf ()
+  "Function for completion at point of AWS CLI services and commands.
+Run \"(add-to-list 'completion-at-point-functions 'awscli-capf)\" in a mode's hook to add this completion."
+  (unless accapf--services-info
+    (accapf--read-data-from-file))
+  (save-excursion
+    (let* ((line (split-string (thing-at-point 'line t)))
+           (bounds (bounds-of-thing-at-point 'sexp)) ;; 'word is delimited by "-" in shell modes, 'sexp is "space delimited" like we want
+           (is-aws-command (string= (first line) "aws"))
+           (service (second line))
+           (command (third line))
+           (params (accapf--param-strings-only line)) ;; parameters start with --, we use this to filter parameters already consumed
+           (service-names-docs (accapf--service-completion-data)) ;; we always need the service names to confirm we have a good match
+           (command-names-docs (accapf--command-completion-data service)) ;; will return data for a "good" service name, or nil for a partial/invalid entry
+           (candidates nil)) ;; populated in the cond below
+      (when is-aws-command
+              ;; TODO
+        (cond ((and service (member command command-names-docs)) (setq candidates (accapf--parameters-completion-data service command params)))
+              ;; TODO
+              ((and service (member service service-names-docs)) (setq candidates command-names-docs))
+              ;; if it's aws command but there's no match for service name, complete service
+              (t (setq candidates service-names-docs)))
+        (when bounds
+          (list (car bounds)
+                (cdr bounds)
+                candidates
+                :exclusive 'no
+                :company-docsig #'identity
+                :company-doc-buffer (lambda (the-candidate)
+                                      ;; this property is added to the name string in the function that get the completion data for `candidates'
+                                      (company-doc-buffer (get-text-property 0 :awsdoc the-candidate)))))))))
+
+(defun accapf--param-strings-only (strings)
+  (cl-remove-if-not (lambda (str) (string-prefix-p "--" str)) strings))
+
+(defun accapf--service-completion-data ()
+  (mapcar (lambda (serv)
+            (propertize (accapf--service-name serv)
+                        :awsdoc (accapf--service-docs serv)))
+          accapf--services-info))
+
+(defun accapf--command-completion-data (service-name)
+  (let ((service (cl-find service-name
+                          accapf--services-info
+                          :test (lambda (value item)
+                                  (string= (accapf--service-name item) value)))))
+    (when service
+      (mapcar (lambda (comm)
+                (propertize (accapf--command-name comm)
+                            :awsdoc (accapf--command-docs comm)))
+              (accapf--service-commands service)))))
+
+(defun accapf--parameters-completion-data (service-name command-name used-params)
+  (let* ((service (cl-find service-name
+                           accapf--services-info
+                           :test (lambda (value item)
+                                   (string= (accapf--service-name item) value))))
+         (command (when service
+                    (cl-find command-name
+                             (accapf--service-commands service)
+                             :test (lambda (value item)
+                                     (string= (accapf--command-name item) value))))))
+    (when command
+      (cl-remove-if (lambda (item) (member item used-params))
+                    (mapcar (lambda (opt)
+                              (propertize (accapf--option-name opt)
+                                          :awsdoc   (format "Type: %s\n\n%s"
+                                                            (accapf--option-type opt)
+                                                            (accapf--option-docs opt))))
+                            (concatenate 'list
+                                         (accapf--command-options command)
+                                         accapf--global-options-info))))))
+
+(defun accapf-refresh-data-from-cli ()
+  "Run \"aws help\" in the shell and keep walking down all the services and commands to updated the cached docs."
+  (interactive)
+  (with-temp-buffer
+    (call-process "aws" nil t nil "help")
+    (goto-char (point-min))
+    (let* ((case-fold-search nil)
+           (opt-start (search-forward-regexp "^Options$"))
+           (serv-start (search-forward-regexp "^Available Services$"))
+           (serv-end (search-forward-regexp "^See Also$"))
+           (global-options nil)
+           (services nil))
+      ;; from the "Options" title, search for all the ocurrences
+      ;; of "--something-something", bound to the start of services names
+      ;; and retrieve from the line the text between quotes
+      (goto-char opt-start)
+      (while (search-forward-regexp "^\"\\(.*?\\)\" (\\(.*?\\))\n\n\\(.*\\)" serv-start t)
+        (push (accapf--option-create :name (match-string 1)
+                                      :type (match-string 2)
+                                      :docs (match-string 3))
+              global-options))
+      ;; from the "Available Services" title, search for all the ocurrences
+      ;; of "* something", bound to the start the "See Also" title
+      ;; and retrieve from the line the text after "* "
+      (goto-char serv-start)
+      (while (search-forward-regexp "^* \\(.*\\)$" serv-end t)
+        (let ((service-name (match-string 1)))
+          (unless (string= service-name "help")
+            (push (accapf--service-data-from-cli service-name)
+                  services))))
+      (accapf--store-data-in-file (list services global-options)))))
+
+(defun accapf--service-data-from-cli (service)
+  (with-temp-buffer
+    (message "Service: %s" service)
+    (call-process "aws" nil t nil service "help")
+    (goto-char (point-min))
+    (let* ((case-fold-search nil)
+           (command-start (search-forward-regexp "^Available Commands$" nil t))
+           (commands nil))
+      ;; from the "Available Commands" title, search for all the ocurrences
+      ;; of "* something" until the end of the buffer, and retrieve
+      ;; from the line the text after "* "
+      (when command-start
+        (goto-char command-start)
+        (while (search-forward-regexp "^* \\(.*\\)$" nil t)
+          (let ((command-name (match-string 1)))
+            (unless (string= command-name "help") ;; yeah, skip "help"
+              (push (accapf--command-data-from-cli service command-name)
+                    commands)))))
+      ;; return the service, use the entire buffer as help string
+      (accapf--service-create :name service
+                               :commands commands
+                               :docs (buffer-string)))))
+
+(defun accapf--command-data-from-cli (service command-name)
+  (with-temp-buffer
+    (message "Service: %s Command: %s" service command-name)
+    (call-process "aws" nil t nil service command-name "help")
+    (goto-char (point-min))
+    (let* ((case-fold-search nil)
+           (opt-start (search-forward-regexp "^Options$" nil t))
+           (options nil))
+      ;; from the "Options" title, search for all the ocurrences
+      ;; of "--something-something", bound to the start of services names
+      ;; and retrieve from the line the text between quotes
+      ;; some commands don't have "Options", for now we ignore them but
+      ;; there's a chance that handling will be added to them
+      (when opt-start
+        (goto-char opt-start)
+        (while (search-forward-regexp "^\"\\(.*?\\)\" (\\(.*?\\))\n\n\\(.*\\)" nil t)
+          (push (accapf--option-create :name (match-string 1)
+                                        :type (match-string 2)
+                                        :docs (match-string 3))
+                options)))
+      (accapf--command-create :name command-name
+                               :options options
+                               :docs (buffer-string)))))
+
+;; (defun accapf--log (&rest to-log)
+;;   "Append TO-LOG to the log buffer.  Intended for internal use only."
+;;   (let ((log-buffer (get-buffer-create "*accapf-log*"))
+;;         (text (cl-reduce (lambda (accum elem) (concat accum " " (prin1-to-string elem t))) to-log)))
+;;     (with-current-buffer log-buffer
+;;       (goto-char (point-max))
+;;       (insert text)
+;;       (insert "\n"))))
